83c47c61895c4fde54d3662cdd2f63a83af2d1bd
Jacob Taylor <jacobjtaylor0@gmail.com>
2023-11-14 20:48:48 -0500

    Added template.html and landing page

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, send_from_directory
 import os
 from dotenv import load_dotenv
 from models import User, db
 
 load_dotenv()
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/rabbithole/static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
 db.init_app(app)
 
+@app.route('/rabbithole/static/<path:path>')
+def send_static(path):
+    return send_from_directory('static', path)
+
 @app.get('/')
 def index():
-    return 'Hello World'
+    return render_template('landing_page.html')
